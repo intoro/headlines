8e5efd795d7bc09684917185215871638c5a68b4
B <mayrose.dg@gmail.com>
2017-03-05 03:33:14 -0500

    Using GET

--- headlines.py
+++ headlines.py
@@ -1,6 +1,7 @@
 import feedparser
 from flask import Flask
 from flask import render_template
+from flask import request
 
 
 app = Flask(__name__)
@@ -9,22 +10,30 @@ RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
              'cnn': 'http://rss.cnn.com/rss/edition.rss',
              'fox': 'http://feeds.foxnews.com/foxnews/latest',
              'iol': 'http://www.iol.co.za/cmlink/1.640',
+             'ti': 'http://www.businessinsider.com/sai/rss',
+             'bi': 'http://uk.businessinsider.com/rss',
              'hn' : 'https://news.ycombinator.com/rss'}
 
 
 
-
-
-
 @app.route("/")
-@app.route("/<publication>")
-def get_news(publication="hn"):
-  feed = feedparser.parse(RSS_FEEDS[publication])
-  return render_template("home.html", articles=feed['entries'])
+def get_news():
+        query = request.args.get("publication")
+        if not query or query.lower() not in RSS_FEEDS:
+                publication = "bbc"
+        else:
+                publication = query.lower()
+        feed = feedparser.parse(RSS_FEEDS[publication])
+        return render_template("home.html",articles=feed['entries'])
+
+
+
+if __name__ == "__main__":
+    app.run(port=5000, debug=True)
 
 
 
 
 
-if __name__ == '__main__':
-  app.run(port=5000, debug=True)
+
+# kool
